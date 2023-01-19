eb096b5bc36db9823efdc8af05b55c58fa11b245
M P <potyomkin.m@gmail.com>
2023-01-19 20:04:47 +0100

    1-module\3-task solution

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,12 @@
 function ucFirst(str) {
-  // ваш код...
+  let newStr;
+  if (str.length > 1) {
+    newStr = str[0].toUpperCase() + str.slice(1);
+  } else if (str.length === 1) {
+    newStr = str[0].toUpperCase()
+  } else if (str === '') {
+    newStr = str;
+  }
+
+  return newStr;
 }
