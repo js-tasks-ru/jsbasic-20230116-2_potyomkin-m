d288545cbed916c9fea22ae615f195f43841c9ae
M P <potyomkin.m@gmail.com>
2023-01-20 13:45:46 +0100

    1-module\2,4-task solutions update

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,9 +1,16 @@
 function truncate(str, maxlength) {
-  let newStr;
-  if (str.length > maxlength) {
-    newStr = str.slice(0, maxlength-1) + '…';
-  } else {
-    newStr = str;
+  // let newStr;
+  // if (str.length > maxlength) {
+  //   newStr = str.slice(0, maxlength-1) + '…';
+  // } else {
+  //   newStr = str;
+  // }
+  // return newStr;
+
+  if (str.length <= maxlength) {
+    return str;
   }
-  return newStr;
+
+  return str.slice(0, maxlength - 1) + '…';
+
 }
