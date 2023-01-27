255b01e2086c547918e8507fe500e4384a7a4d07
M P <potyomkin.m@gmail.com>
2023-01-27 13:58:46 +0100

    4-module solution

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let wrapper = document.createElement('ul');
+
+  friends.forEach(friend => {
+    let li = document.createElement('li');
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    wrapper.appendChild(li);
+  });
+  
+  return wrapper;
 }
