962e5c3950fc025db3900a34d31ea3575cc7564c
M P <potyomkin.m@gmail.com>
2023-02-09 16:50:36 +0100

    5-module solution

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -13,7 +13,7 @@ function highlight(table) {
     // }
 
     row.cells[3].dataset.available === 'false' ? row.cells[3].parentNode.classList.add('unavailable') : row.cells[3].dataset.available === undefined ? row.cells[3].parentNode.setAttribute('hidden', '') : row.cells[3].parentNode.classList.add('available') ;
-  
+
     row.cells[2].innerHTML === 'm' ? row.cells[2].parentNode.classList.add('male') : row.cells[2].parentNode.classList.add('female'); 
 
     +row.cells[1].innerHTML < 18 ? row.cells[1].parentNode.style.textDecoration = 'line-through' : row.cells[1].parentNode.style.textDecoration = ''; 
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,57 @@
 function initCarousel() {
-  // ваш код...
+  const carouselArrows = document.querySelectorAll('.carousel__arrow ');
+
+  const carousel = document.querySelector('.carousel__inner');
+  const slides = carousel.querySelectorAll('.carousel__slide');
+  const slidesLength = slides.length - 1;
+
+  let currentSlide = 0;
+
+  function slidesActualization(currentSlide, slidesLength) {
+    
+    if (currentSlide <= 0) {
+      currentSlide = 0;
+      document.querySelector('.carousel__arrow_left').style.display = 'none';
+    } else {
+      document.querySelector('.carousel__arrow_left').style.display = '';      
+    }
+
+
+    if (currentSlide >= slidesLength) {
+
+      currentSlide = slidesLength;
+      document.querySelector('.carousel__arrow_right').style.display = 'none';
+    } else {
+      document.querySelector('.carousel__arrow_right').style.display = '';
+    }
+
+    carousel.style.transform = `translateX(${-slides[currentSlide].offsetWidth * (currentSlide)}px)`;
+
+  }
+
+
+  slidesActualization(currentSlide, slidesLength);
+
+  carouselArrows.forEach((carouselArrow) => {
+
+    carouselArrow.addEventListener('click', () => {
+
+      if (carouselArrow.classList.contains('carousel__arrow_left')) {
+        --currentSlide;
+        
+      }
+      if (carouselArrow.classList.contains('carousel__arrow_right')) {
+        ++currentSlide;
+      }
+
+      slidesActualization(currentSlide, slidesLength);
+
+    })
+
+
+  });
+
+
+
+  
 }
