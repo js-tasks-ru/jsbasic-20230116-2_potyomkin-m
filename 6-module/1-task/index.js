6df8c61f79deeaf6d0eb4a3eb1e1a60f5db333f1
M P <potyomkin.m@gmail.com>
2023-02-12 15:25:22 +0100

    6-module\1-task solution

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,49 @@
  *
  */
 export default class UserTable {
+  elem = null;
+  
   constructor(rows) {
+   
+    let tbl = document.createElement('table');
+   
+    let inner = `
+    <thead>
+      <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+      </tr >
+    </thead >
+    <tbody>`;
+    
+    rows.forEach(row => {
+      inner += `<tr>`;
+      for (let key in row) {       
+        inner += `<td>${row[key]}</td>`;
+      }
+      inner += `<td><button>X</button></td></tr>`;
+      
+    });
+
+    inner += `</tbody>`;
+
+    tbl.innerHTML = inner;
+    
+    this.elem = tbl;
+
+    const buttons = tbl.querySelectorAll('button');
+    
+    buttons.forEach(button => {
+      button.addEventListener('click', this.#onButtonClick);
+    });
+
+    
   }
+  #onButtonClick = (e) => {
+    e.target.closest('tr').remove();
+  }
+
 }
